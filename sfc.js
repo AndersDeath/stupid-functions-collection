ddbe608e360e01a72b0ded1f37a5bfa37a561323
VNB <andersdeath@yandex.ru>
2020-08-19 21:07:53 +0300

    add typesToString func

---
+++ sfc.js
@@ -0,0 +1,9 @@
+const typesToString = (containerId, examplesObj) => {
+    let container = document.getElementById(containerId);
+    let keys = Object.keys(examplesObj);
+    for(let i = 0; i < keys.length; i++) {
+      let el = document.createElement('div');
+      el.innerHTML = `<strong>${keys[i]}</strong> - ${String(examplesObj[keys[i]])}`
+      container.appendChild(el);
+    }
+  }
