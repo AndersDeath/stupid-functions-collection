abd677bfc6816209858bc18579d1efb73b0ceb59
VNB <andersdeath@yandex.ru>
2020-08-22 22:47:16 +0300

    update to v2

--- sfc.js
+++ sfc.js
@@ -1,19 +1,31 @@
-const typesToString = (containerId, examplesObj) => {
-    let container = document.getElementById(containerId);
-    let keys = Object.keys(examplesObj);
-    for(let i = 0; i < keys.length; i++) {
-      let el = document.createElement('div');
-      el.innerHTML = `<strong>${keys[i]}</strong> - ${String(examplesObj[keys[i]])}`
-      container.appendChild(el);
-    }
-  }
-
-const typesToBoolean = (containerId, examplesObj) => {
-  let container = document.getElementById(containerId);
+const buildTheList = (examplesObj, to) => {
+  let body = document.querySelector('body');
+  let container = document.createElement('div');
+  body.appendChild(container);
   let keys = Object.keys(examplesObj);
   for(let i = 0; i < keys.length; i++) {
     let el = document.createElement('div');
-    el.innerHTML = `<strong>${keys[i]}</strong> - ${Boolean(examplesObj[keys[i]])}`
+    el.innerHTML = `<strong>${keys[i]}</strong> - ${to(examplesObj[keys[i]])}`
     container.appendChild(el);
   }
 }
+
+const typesToString = (examplesObj) => {
+  buildTheList(examplesObj, String)
+}
+
+const typesToBoolean = (examplesObj) => {
+  buildTheList(examplesObj, Boolean)
+}
+
+const typesToNumber = (examplesObj) => {
+  buildTheList(examplesObj, Number)
+}
+
+const sfc = {
+  typesToString,
+  typesToBoolean,
+  typesToNumber
+}
+
+window.sfc = sfc;
