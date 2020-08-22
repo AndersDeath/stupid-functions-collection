326eb5080b8db8ca050d02abadf167511754630b
VNB <andersdeath@yandex.ru>
2020-08-22 09:08:04 +0300

    add types to boolean

--- sfc.js
+++ sfc.js
@@ -7,3 +7,13 @@ const typesToString = (containerId, examplesObj) => {
       container.appendChild(el);
     }
   }
+
+const typesToBool = (containerId, examplesObj) => {
+  let container = document.getElementById(containerId);
+  let keys = Object.keys(examplesObj);
+  for(let i = 0; i < keys.length; i++) {
+    let el = document.createElement('div');
+    el.innerHTML = `<strong>${keys[i]}</strong> - ${Boolean(examplesObj[keys[i]])}`
+    container.appendChild(el);
+  }
+}
